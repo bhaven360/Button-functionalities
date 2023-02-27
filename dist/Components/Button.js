7ed4623f8368866200bf6702101f8507f041e887
bhaven360 <bhaven.jain@careers360.in>
2023-02-27 13:22:46 +0530

    changes

---
+++ dist/Components/Button.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+require("./styles/Button.css");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const Button = _ref => {
+  let {
+    className,
+    type,
+    text
+  } = _ref;
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: className,
+    type: type
+  }, text);
+};
+var _default = Button;
+exports.default = _default;
