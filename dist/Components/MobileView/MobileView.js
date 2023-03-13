3b0e37373d935552e8a09e229e355ed980904a5d
bhaven360 <bhaven.jain@careers360.in>
2023-03-13 11:10:55 +0530

    changes

---
+++ dist/Components/MobileView/MobileView.js
@@ -0,0 +1,42 @@
+"use strict";
+
+require("core-js/modules/es.weak-map.js");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+require("core-js/modules/web.dom-collections.iterator.js");
+require("core-js/modules/es.regexp.exec.js");
+require("core-js/modules/es.regexp.test.js");
+var _react = _interopRequireWildcard(require("react"));
+var _skeleton = _interopRequireDefault(require("../styles/skeleton.css"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+const MobileView = props => {
+  const [deviceType, setDeviceType] = (0, _react.useState)("");
+  (0, _react.useEffect)(() => {
+    let hasTouchScreen = false;
+    if ("maxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.maxTouchPoints > 0;
+    } else if ("msMaxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.msMaxTouchPoints > 0;
+    } else {
+      const mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+      if (mQ && mQ.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+      } else if ("orientation" in window) {
+        hasTouchScreen = true;
+      } else {
+        const UA = navigator.userAgent;
+        hasTouchScreen = /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+      }
+    }
+    hasTouchScreen ? setDeviceType("Mobile") : setDeviceType("Desktop");
+  }, []);
+  return deviceType === "Mobile" ? /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, props.children) : props.skeleton && /*#__PURE__*/_react.default.createElement("div", {
+    className: _skeleton.default.skeleton
+  }, props.children);
+};
+var _default = MobileView;
+exports.default = _default;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,5 @@
 import Button from "./Components/Button";
+import BrowserView from "./Components/BrowserView/BrowserView.jsx";
+import MobileView from "./Components/MobileView/MobileView.jsx";
 
-export {Button};
+export { Button, BrowserView, MobileView };
